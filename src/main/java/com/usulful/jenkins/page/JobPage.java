caca1bcf1aa121b7789b0186e80ad3dea9713e3d
Ryszard Perkowski <usultis@gmail.com>
2013-01-27 00:58:26 +0100

    adding waiting for title change

--- src/main/java/com/usulful/jenkins/page/JobPage.java
+++ src/main/java/com/usulful/jenkins/page/JobPage.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import static org.junit.Assert.assertThat;
 import static org.junit.matchers.JUnitMatchers.containsString;
@@ -36,7 +38,7 @@ public class JobPage {
     }
 
     public JobPage assertTitleContains(final String titlePart) {
-        assertThat(driver.getTitle(), containsString(titlePart));
+        new WebDriverWait(driver, 5).until(ExpectedConditions.titleContains(titlePart));
         return this;
     }
 }
